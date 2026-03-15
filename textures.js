86efa69bb899530a5f0e64539f474f5274cc1f58
mcassets <joseph.1555235@nv.ccsd.net>
2026-03-15 14:40:52 -0700

    Create textures.js

---
+++ textures.js
@@ -0,0 +1,8 @@
+textures: [
+  'https://raw.githubusercontent.com/mcassets/textures/refs/heads/main/grass_block_side.png', // 0: Right (+X)
+  'https://raw.githubusercontent.com/mcassets/textures/refs/heads/main/grass_block_side.png', // 1: Left (-X)
+  'https://raw.githubusercontent.com/mcassets/textures/refs/heads/main/grass_block_top.png',  // 2: Top (+Y)
+  'https://raw.githubusercontent.com/mcassets/textures/refs/heads/main/dirt.png',             // 3: Bottom (-Y)
+  'https://raw.githubusercontent.com/mcassets/textures/refs/heads/main/grass_block_side.png', // 4: Front (+Z)
+  'https://raw.githubusercontent.com/mcassets/textures/refs/heads/main/grass_block_side.png'  // 5: Back (-Z)
+],
