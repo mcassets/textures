6a7923bc5cc1c675efa7ae57cea6c23e94b9e415
mcassets <joseph.1555235@nv.ccsd.net>
2026-03-15 17:29:37 -0700

    Create textures full.js

---
+++ textures full.js
@@ -0,0 +1,16 @@
+textures: [
+                'https://raw.githubusercontent.com/mcassets/textures/refs/heads/main/grass_block_side.png', // +X
+                'https://raw.githubusercontent.com/mcassets/textures/refs/heads/main/grass_block_side.png', // -X
+                'https://raw.githubusercontent.com/mcassets/textures/refs/heads/main/grass_block_top.png',  // +Y
+                'https://raw.githubusercontent.com/mcassets/textures/refs/heads/main/dirt.png',            // -Y
+                'https://raw.githubusercontent.com/mcassets/textures/refs/heads/main/grass_block_side.png', // +Z
+                'https://raw.githubusercontent.com/mcassets/textures/refs/heads/main/grass_block_side.png'  // -Z
+            ],
+            overlays: [
+                'https://raw.githubusercontent.com/mcassets/textures/refs/heads/main/grass_side_overlay.png',
+                'https://raw.githubusercontent.com/mcassets/textures/refs/heads/main/grass_side_overlay.png',
+                null, 
+                null,
+                'https://raw.githubusercontent.com/mcassets/textures/refs/heads/main/grass_side_overlay.png',
+                'https://raw.githubusercontent.com/mcassets/textures/refs/heads/main/grass_side_overlay.png'
+            ]
